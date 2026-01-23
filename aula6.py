347991ba1aad19208a6d383f95521b5b25607081
Pedro-gomes123 <pcg@cesar.schoolgit config --global user.name>
2026-01-22 21:34:42 -0300

    criação e implementação da tabelas usando sql, unificação em uma planilia so e iniciando o tratamento

--- aula6.py
+++ aula6.py
@@ -13,20 +13,20 @@ print("--- 1. Buscando dados Brutos ---")
 # TABELA A: Itens da Fatura (InvoiceLine)
 # Precisamos saber o ID da música (TrackId), o Preço (UnitPrice) e a Quantidade (Quantity)
 # Dica: SELECT ... FROM InvoiceLine
-query_itens = "..."
+query_itens = " SELECT TrackId, UnitPrice, Quantity FROM InvoiceLine; "
 df_itens = pd.read_sql_query(query_itens, conexao)
 
 # TABELA B: Músicas (Track)
 # Precisamos do ID da música (TrackId), do Nome da Música (Name) e do ID do Gênero (GenreId)
 # Dica: SELECT ... FROM Track
-query_tracks = "..."
+query_tracks = " SELECT TrackId , Name, GenreId FROM Track;"
 df_tracks = pd.read_sql_query(query_tracks, conexao)
 
 # TABELA C: Gêneros (Genre)
 # Precisamos do ID (GenreId) e do Nome do Gênero (Name)
 # Dica Importante: No SQL, renomeie a coluna Name para 'Nome_Genero' (usando AS)
 # para não confundir com o nome da música depois.
-query_genres = "..."
+query_genres = " SELECT GenreID, Name AS Nome_Genero FROM Genre; "
 df_genres = pd.read_sql_query(query_genres, conexao)
 
 conexao.close()
@@ -39,20 +39,24 @@ print("\n--- 2. Tratando os Dados ---")
 
 # PASSO A: Criar Coluna de Faturamento Total por Item
 # No Pandas, multiplique a coluna UnitPrice pela coluna Quantity dentro do df_itens
-# df_itens['Total_Item'] = ...
 
+
+df_itens['Total Vendido'] = df_itens['UnitPrice'] * df_itens['Quantity']
+print(df_itens)
 
 # PASSO B: Primeiro Merge (Itens + Músicas)
 # Junte df_itens com df_tracks usando a chave 'TrackId'
-# df_merge1 = ...
 
+df_marge1 = pd.merge(df_itens, df_tracks, left_on='TrackId', right_on='TrackId')
+print(df_marge1)
 
 # PASSO C: Segundo Merge (Resultado + Gêneros)
 # Junte df_merge1 com df_genres usando a chave 'GenreId'
-# df_final = ...
 
-# Check: Mostre as colunas para ver se tem 'Nome_Genero', 'Name' (música) e 'Total_Item'
-# print(df_final.columns)
+df_final = pd.merge(df_marge1, df_genres, left_on='GenreId', right_on='GenreId')
+print(df_final)
+
+
 
 
 # ==============================================================================
@@ -61,8 +65,8 @@ print("\n--- 2. Tratando os Dados ---")
 
 print("\n--- PERGUNTA 1: Qual Gênero dá mais Lucro? ---")
 # Agrupe df_final por 'Nome_Genero', some a coluna 'Total_Item' e ordene (desc).
-# top_generos = ...
-# print(top_generos.head(5))
+Lucro = df_final.groupby('Nome_Genero')['Total Vendido'].sum().sort_values(ascending=False)
+print(Lucro)
 
 
 print("\n--- PERGUNTA 2: Drill-Down (Aprofundamento) ---")
